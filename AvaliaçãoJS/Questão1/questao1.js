1e72fb03e30cb59d85ccb1baaaad53b402649abb
Yuri Christian <yuri-tech@outlook.com>
2020-10-08 18:32:32 -0300

    Alteração de alguns arquivos na AvaliaçãoJS

---
+++ AvaliaçãoJS/Questão1/questao1.js
@@ -0,0 +1,18 @@
+function mediaAritrimetrica(){
+    let valor1 = document.media.valor1.value;
+    let valor2 = document.media.valor2.value;
+    let valor3 = document.media.valor3.value;
+    let valores = [parseInt(valor1),parseInt(valor2),parseInt(valor3)]
+    function soma(acum,valor){
+        return acum + valor
+    }
+    let media = valores.reduce(soma)/3;
+    document.getElementById("txtResultado").innerHTML = "A média aritmética é: " + media;
+}
+function reset(){
+    document.getElementById("txtResultado").innerHTML = '';
+    document.media.valor1.value = '';
+    document.media.valor2.value = '';
+    document.media.valor3.value = '';
+    
+}
---
+++ AvaliaçãoJS/Questão3/questao3.js
@@ -0,0 +1,20 @@
+function validar(){
+    let valorA = document.validacao.valorA.value;
+    let valorB = document.validacao.valorB.value;
+    document.getElementById("txtResultado").innerHTML = validador(parseInt(valorA),parseInt(valorB));
+
+}
+function validador(a,b){
+    let valorC = 5;
+    if(a == b){
+        valorC = "A e B são iguai\nProtanto a soma dos valores é: " + (parseInt(a) + parseInt(b));
+    }else{
+        valorC = "A e B são diferentes\nProtanto a multiplicação dos valores é: " + (parseInt(a) * parseInt(b));
+    }
+    return valorC;
+}
+function reset(){
+    document.validacao.valorA.value = '';
+    document.validacao.valorB.value = '';
+    document.getElementById("txtResultado").innerHTML = '';
+}
